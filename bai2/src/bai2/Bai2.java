a0cce7d7b512181e031189b82c62d779cd57f975
Hellhihi <Hellhihi@DESKTOP-4LF28QA.net.fpt>
2019-09-26 19:11:39 +0700

    52

--- bai2/src/bai2/Bai2.java
+++ bai2/src/bai2/Bai2.java
@@ -14,7 +14,11 @@ public class Bai2 {
         float c = scanner.nextFloat();
         if (a == 0) {
             if (b == 0) {
-                System.out.println("phuong trinh vo nghiem");
+                if (c == 0) {
+                    System.out.println("phuong trinh co vo so nghiem");
+                } else {
+                    System.out.println("phuong trinh vo nghiem");
+                }
             } else {
                 System.out.println("phuong trinh co nghiem duy nhat: x= " + (-c / b));
             }
