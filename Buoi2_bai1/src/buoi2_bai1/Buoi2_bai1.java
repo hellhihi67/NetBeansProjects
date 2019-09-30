e7b61ddb67e9e05f483ee2b64704afd58f531427
hellhihi67 <hellhihi67@gmail.com>
2019-09-30 14:17:32 +0700

    buoi2

--- Buoi2_bai1/src/buoi2_bai1/Buoi2_bai1.java
+++ Buoi2_bai1/src/buoi2_bai1/Buoi2_bai1.java
@@ -9,16 +9,21 @@ public class Buoi2_bai1 {
         Scanner sc = new Scanner(System.in);
         System.out.print("so tu nhien la: ");
         int A = sc.nextInt();
+        int count;
 
-        if (A < 2) {
+        if (A < 3) {
             System.out.println("khong co so nguyen to nao thoa man");
-        }
-        for (int i = 2; i < A; i++) {       // i la so nguyen to
-            for (int j = 3; j < i; j++) {  // j la uoc cua i
-                if (i % j != 0) {
-
-                    System.out.println(i + ", ");
-
+        } else {
+            System.out.print("day so nguyen to thoa man la: ");
+            for (int i = 2; i < A; i++) {       // i la so nguyen to
+                count = 0;
+                for (int j = 1; j <= i; j++) {  // j la uoc cua i
+                    if (i % j == 0) {
+                        count++;
+                    }
+                }
+                if (count == 2) {
+                    System.out.print(i + "; ");
                 }
             }
         }
--- Buoi2_bai2/src/buoi2_bai2/Buoi2_bai2.java
+++ Buoi2_bai2/src/buoi2_bai2/Buoi2_bai2.java
@@ -9,18 +9,17 @@ public class Buoi2_bai2 {
         System.out.print("hai so nguyen duong ban dau la:" + " ");
         int a = sc.nextInt();
         int b = sc.nextInt();
-        int tram = b / 100;
-        int chuc = b % 100 / 10;
-        int dvi = b % 10;
-        System.out.println(chuc);
+        int tramb = b / 100;
+        int chucb = b % 100 / 10;
+        int dvib = b % 10;      
         System.out.println("mo phong phep nhan tay:");
         System.out.println("     " + a);
         System.out.println("    x");
         System.out.println("     " + b);
         System.out.println("------------");
-        System.out.println("    " + a * dvi);
-        System.out.println("   " + a * chuc);
-        System.out.println("  " + a * tram);
+        System.out.println("    " + a * dvib);
+        System.out.println("   " + a * chucb);
+        System.out.println("  " + a * tramb);
         System.out.println("------------");
         System.out.println("  " + a * b);
     }
