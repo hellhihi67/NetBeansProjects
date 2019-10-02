b38094162d667aa220eb3675645a4e75dc5bb79d
Hellhihi <Hellhihi@DESKTOP-4LF28QA.net.fpt>
2019-10-02 11:10:33 +0700

    ddd

--- BTVN_buoi2/src/btvn_buoi2/bai3.java
+++ BTVN_buoi2/src/btvn_buoi2/bai3.java
@@ -16,6 +16,20 @@ public class bai3 {
         for (int i = 0; i < 5; i++) {
             System.out.print(arr[i] + ", ");
         }
-
+        int temp = arr[0];
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] < arr[j]) {
+                    temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                    System.out.print(temp + ", ");
+                }
+            }
+        }
+        System.out.print("day so tang dan la: ");
+        for (int i = 0; i < 5; i++) {
+            System.out.print(arr[i] + ", ");
+        }
     }
 }
